77b8516411bfad03c21e4c2ef37a5cab143fdddb
Justin Warren <jw402216@ohio.edu>
2024-11-04 08:38:53 -0500

    Update dumper_constants.h
    Changed a couple of constants

--- include/dumper_constants.h
+++ include/dumper_constants.h
@@ -5,8 +5,8 @@
 #ifndef DUMPERCONSTS
 #define DUMPERCONSTS
 
-static const size_t maxfilesize = (size_t)(0.1*(sysconf(_SC_PHYS_PAGES) * sysconf(_SC_PAGE_SIZE)));
-static const size_t maxread = (size_t)(0.1 * maxfilesize);
+static const size_t maxfilesize = (size_t)(0.5*(sysconf(_SC_PHYS_PAGES) * sysconf(_SC_PAGE_SIZE)));
+static const size_t maxread = (size_t)(0.05 * maxfilesize);
 static const char* installpath = std::getenv("DUMPERSYS");
 
 #endif
